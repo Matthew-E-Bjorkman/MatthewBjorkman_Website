4fbb2b28dc0dd8fa72fd6b3a7a563156a781b005
Matthew Bjorkman <mbjorkman@bigtime.net>
2023-07-01 08:19:59 -0500

    Revert "Config + Email Form + SendGrid integration (wip)"
    This reverts commit eb28ab84c1f66b476e401904dc32d69df381ad97.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,18 @@
-import { APP_INITIALIZER, NgModule } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule, Routes } from '@angular/router';
 import { LocationStrategy, PathLocationStrategy } from '@angular/common';
-import { HttpClientModule } from '@angular/common/http';
-import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 
-import { AppConfigService } from './services/app-config/app-config.service';
-import { EmailGeneratorService } from './services/email-generator/email-generator.service';
+import { NavbarComponent } from './navbar/navbar.component';
 
-import { NavbarComponent } from './ui/navbar/navbar.component';
+import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ContactComponent } from './pages/contact/contact.component';
 
-import { HomeComponent } from './ui/pages/home/home.component';
-import { AboutComponent } from './ui/pages/about/about.component';
-import { ContactComponent } from './ui/pages/contact/contact.component';
-
-import { WIPComponent } from './ui/elements/wip/wip.component';
-import { FooterComponent } from './ui/footer/footer.component';
-import { EmailFormComponent } from './ui/elements/email-form/email-form.component';
+import { WIPComponent } from './elements/wip/wip.component';
+import { FooterComponent } from './footer/footer.component';
 
 
 const routes: Routes = [
@@ -35,24 +29,14 @@ const routes: Routes = [
     ContactComponent,
     NavbarComponent,
     WIPComponent,
-    FooterComponent,
-    EmailFormComponent
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    RouterModule.forRoot(routes),
-    HttpClientModule,
-    ReactiveFormsModule
+    RouterModule.forRoot(routes)
   ],
   providers: [
-    { provide: LocationStrategy, useClass: PathLocationStrategy },
-    {
-      provide: APP_INITIALIZER,
-      useFactory: (appConfigService: AppConfigService) => () => appConfigService.loadConfig(),
-      deps: [AppConfigService],
-      multi: true
-    },
-    EmailGeneratorService
+    { provide: LocationStrategy, useClass: PathLocationStrategy }
   ],
   bootstrap: [AppComponent]
 })
